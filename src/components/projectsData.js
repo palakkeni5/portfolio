915cd6eec8e14a1fdc3ef46086931bccc8465f59
palakkeni5 <palakkeni@gmail.com>
2024-01-10 11:05:41 -0500

    updated projects

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -2,9 +2,27 @@ import weatherAppImage from "../assets/project-images/weather-app.png";
 import pokedexAppImage from "../assets/project-images/pokedex-app.png";
 import portfolioImage from "../assets/project-images/screen-capture-mp4.gif";
 import brickBreakerImage from "../assets/project-images/brick-breaker.png";
+import oneRoomChatAppImage from "../assets/project-images/one-room-chat-app.png";
+import awesomeSuperstoreAppImage from "../assets/project-images/awesome-super-store.png";
 
 export const data = {
   projects: [
+    {
+      projectImage: oneRoomChatAppImage,
+      projectName: "One Room Chat App",
+      projectDescription:
+        "A real-time multi-user chat application with a single chat room that allows users to communicate with each other.",
+      projectTech: ["Java", "React", "PostgreSQL", "Docker"],
+      projectLink: "https://github.com/palakkeni5/one-room-chat-app",
+    },
+    {
+      projectImage: awesomeSuperstoreAppImage,
+      projectName: "Awesome Superstore App",
+      projectDescription:
+        "This project contains the web application, database and ETL components for a business use case to develop an e-commerce web application for Awesome Superstore Inc.",
+      projectTech: ["React", "Node JS", "Oracle SQL", "MySQL"],
+      projectLink: "https://github.com/palakkeni5/awesome-superstore-app",
+    },
     {
       projectImage: weatherAppImage,
       projectName: "Weather App",
@@ -31,13 +49,6 @@ export const data = {
       projectTech: ["React", "Javascript", "HTML", "CSS"],
       projectLink: "https://github.com/palakkeni5/portfolio",
       liveDemo: "https://palakkeni5.github.io/portfolio/",
-    },
-    {
-      projectImage: brickBreakerImage,
-      projectName: "Brick Breaker Game",
-      projectDescription: "A simple brick breaker game developed in java",
-      projectTech: ["Java"],
-      projectLink: "https://github.com/palakkeni5/brick-breaker-game-in-java",
     },
   ],
 };
